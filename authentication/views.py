e86ab870bef7e29c14f973010caecd31be6ee3fa
Weidenthal Sandra <spiritf0rest@hotmail.com>
2023-04-19 17:52:23 +0200

    feat: add authentication part

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+from . import forms
 
-# Create your views here.
+
+class SignUpView(View):
+    template_name = "authentication/signup.html"
+    form_class = forms.SignupForm
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, context={"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return render(request, self.template_name, context={"form": form})
+
+
+class LoginView(View, LoginRequiredMixin):
+    template_name = "authentication/login.html"
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ""
+        return render(request, self.template_name, context={"form": form, "message": message})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+        message = "Identifiants invalides."
+        return render(request, self.template_name, context={"form": form, "message": message})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
