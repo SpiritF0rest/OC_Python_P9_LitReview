58b0e0e25607617f84760c0975cde05edc55f194
Weidenthal Sandra <spiritf0rest@hotmail.com>
2023-05-04 19:00:21 +0200

    feat: add update and delete for ticket and review

--- litreview/urls.py
+++ litreview/urls.py
@@ -11,10 +11,15 @@ urlpatterns = [
     path('login/', authentication.views.LoginView.as_view(), name="login"),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('', reviewapp.views.home, name='home'),
+    path('posts/', reviewapp.views.posts, name='posts'),
     path('tickets/add/', reviewapp.views.TicketCreationView.as_view(), name='ticket-create'),
     path('tickets/<int:ticket_id>/', reviewapp.views.TicketView.as_view(), name='ticket'),
-    path('reviews/add/<int:ticket_id>/', reviewapp.views.ReviewCreationView.as_view(), name='review-create'),
-    path('tickets/reviews/add', reviewapp.views.FullReviewView.as_view(), name='review-ticket-create')
+    path('tickets/<int:ticket_id>/update/', reviewapp.views.update_ticket, name='update-ticket'),
+    path('tickets/<int:ticket_id>/delete/', reviewapp.views.delete_ticket, name='delete-ticket'),
+    path('tickets/<int:ticket_id>/review/add/', reviewapp.views.ReviewCreationView.as_view(), name='review-create'),
+    path('review/<int:review_id>/update/', reviewapp.views.update_review, name='update-review'),
+    path('review/<int:review_id>/delete/', reviewapp.views.delete_review, name='delete-review'),
+    path('tickets/review/add/', reviewapp.views.FullReviewView.as_view(), name='review-ticket-create')
 ]
 
 if settings.DEBUG:
--- reviewapp/views.py
+++ reviewapp/views.py
@@ -1,7 +1,10 @@
+import os
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic import View
 from . import forms, models
+from itertools import chain
 
 
 @login_required
@@ -9,6 +12,14 @@ def home(request):
     tickets = models.Ticket.objects.all()
     reviews = models.Review.objects.all()
     return render(request, 'reviewapp/home.html', context={"tickets": tickets, "reviews": reviews})
+
+
+@login_required
+def posts(request):
+    tickets = models.Ticket.objects.filter(user=request.user)
+    reviews = models.Review.objects.filter(user=request.user)
+    all_posts = sorted(chain(tickets, reviews), key=lambda instance: instance.time_created, reverse=True)
+    return render(request, 'reviewapp/posts.html', context={"posts": all_posts})
 
 
 class TicketCreationView(View):
@@ -82,3 +93,53 @@ class FullReviewView(View):
             review.save()
             return redirect('home')
         return render(request, self.template_name, context={"ticket_form": ticket_form, "review_form": review_form})
+
+
+@login_required
+def update_ticket(request, ticket_id):
+    ticket = models.Ticket.objects.get(id=ticket_id)
+    old_image = ticket.image
+    if request.method == "POST" and request.user == ticket.user:
+        form = forms.TicketForm(request.POST, request.FILES, instance=ticket)
+        if form.is_valid():
+            if old_image:
+                if 'image' not in form.files or ('image' in form.files and form.files["image"] != old_image):
+                    os.remove(old_image.path)
+            form.save()
+            return redirect('posts')
+    else:
+        form = forms.TicketForm(instance=ticket)
+    return render(request, 'reviewapp/update_post.html', {'form': form, "is_ticket": True})
+
+
+@login_required
+def update_review(request, review_id):
+    review = models.Review.objects.get(id=review_id)
+    if request.method == "POST" and request.user == review.user:
+        form = forms.ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
+    else:
+        form = forms.ReviewForm(instance=review)
+    return render(request, 'reviewapp/update_post.html', {'form': form, "is_ticket": False, 'ticket': review.ticket})
+
+
+@login_required
+def delete_ticket(request, ticket_id):
+    ticket = models.Ticket.objects.get(id=ticket_id)
+    if request.method == 'POST' and request.user == ticket.user:
+        if ticket.image:
+            os.remove(ticket.image.path)
+        ticket.delete()
+        return redirect('posts')
+    return render(request, 'reviewapp/delete_post.html', context={"ticket": ticket})
+
+
+@login_required
+def delete_review(request, review_id):
+    review = models.Review.objects.get(id=review_id)
+    if request.method == 'POST' and request.user == review.user:
+        review.delete()
+        return redirect('posts')
+    return render(request, 'reviewapp/delete_post.html', context={"review": review})
